5948de4babd1b4dead18092d33535fd967381d41
Brian Haberer <bhaberer@gmail.com>
2013-05-04 18:01:39 -0700

    first commit

---
+++ lib/cinch-weatherman.rb
@@ -0,0 +1,7 @@
+require "cinch-weatherman/version"
+
+module Cinch
+  module Weatherman
+    # Your code goes here...
+  end
+end
---
+++ lib/cinch-weatherman/version.rb
@@ -0,0 +1,5 @@
+module Cinch
+  module Weatherman
+    VERSION = "0.0.1"
+  end
+end
