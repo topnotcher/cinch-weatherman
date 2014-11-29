73b60eec69cc788b7e8f8b7d045e8eaa129abeeb
Sam Cassiba <sam@cassiba.com>
2014-11-28 16:15:44 -0800

    change output for temp_fmt to match weather.rb

--- lib/cinch/plugins/weatherman/forecast.rb
+++ lib/cinch/plugins/weatherman/forecast.rb
@@ -15,7 +15,7 @@ module Cinch
           @temp_low_c = @data.low.celsius.round
           @temp_high_f = @data.high.fahrenheit.round
           @temp_low_f = @data.low.fahrenheit.round
-          @temp_fmt = "%dF / %dC"
+          @temp_fmt = "%dF (%dC)"
           @temp_high = "with a high of #{@temp_fmt}" % [@temp_high_f,@temp_high_c]
           @temp_low = "and a low of #{@temp_fmt}." % [@temp_low_f,@temp_low_c]
         end
--- lib/cinch/plugins/weatherman/version.rb
+++ lib/cinch/plugins/weatherman/version.rb
@@ -3,7 +3,7 @@ module Cinch
   module Plugins
     # Versioning Info
     class Weatherman
-      VERSION = '1.0.6'
+      VERSION = '1.0.7'
     end
   end
 end
